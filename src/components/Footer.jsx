1a1c72be1b67cc5d88d0632e35ed09474cfd1c92
Murilo Bispo <murilooliveirabispo90@gmail.com>
2023-05-17 14:36:43 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,51 +4,37 @@ import {AiFillGithub, AiFillYoutube} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <footer className="footer">
-    <div className="container-footer">
-      <div className="row">
-        <div className="footer-col">
-          <a href="#">Sobre</a>
+    <div className="Footer">
+            <footer>
+                <div class="caixa-footer">
+                    <div class="links-footer">
+                        <nav>
+                            <ul>
+                                <li><a href="">Sobre</a></li>
+                                <li><a href="">Notícias</a></li>
+                                <li><a href="">Pro</a></li>
+                                <li><a href=""> Retrospectiva</a></li>
+                                <li><a href="">Ajuda</a></li>
+                                <li><a href="">Contato</a></li>
+                            </ul>
+                        </nav>
+                        <p class="copyright">&copy; Ciné. Feito por uma equipe de estudantes. Informações sobre os filmes providenciadas pela TMDb.</p>
+                    </div>
+                    <div class ="segundo-container-footer">
+                        <div class="redes-sociais">
+                            <ul>
+                                <li> <a href=""><img class="img-redes-sociais img-ytb" src="images/youtube-icon.png" alt="youtube"></img></a></li>
+                                <li> <a href=""><img class="img-redes-sociais img-github" src="images/github-icon.png"alt="github"></img></a></li>
+                            </ul>
+                        </div>
+                        <div class="equipe-de-dev">
+                            <p>Desenvolvido por</p>
+                            <img class="img-equipe-de-dev" src="images/sapucai-socket-logo.png" alt="sapucai socket"></img>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <div className="footer-col">
-          <a href="#">Notícias</a>
-        </div>
-        <div className="footer-col">
-          <a href="#">Pro</a>
-        </div>
-        <div className="footer-col">
-          <a href="#">Retro</a>
-        </div>
-        <div className="footer-col">
-          <a href="#">Ajuda</a>
-        </div>
-        <div className="footer-col">
-          <a href="#">Contato</a>
-        </div>
-        <div className='footer-col footer-social-links'>
-            <ul>
-                <li>
-                    <AiFillGithub />
-                </li>
-                <li>
-                    <AiFillYoutube />
-                </li>
-                <li>
-                    <img className="img-sapucai" src="./public/images/SAPUCAÍ_BBack.png" alt="" href="" />
-                </li>
-            </ul>
-        </div>
-        <div className="footer-col">
-          
-        </div>
-      </div>
-      <h4 className="copy">
-        © Ciné. Feito por uma equipe de estudantes. Informações sobre os filmes
-        providenciadas pela TMDb.
-      </h4>
-    </div>
-  </footer>
-  
   )
 }
 
